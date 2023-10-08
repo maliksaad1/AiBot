c52a06cfdcb05376bace5bea8a48f9a45b93ab30
Malik Muhammad Saad <saad.shafiq11052004@gmail.com>
2023-10-08 14:19:30 +0500

    Update Forex Price Prediction.py

--- Forex Price Prediction.py
+++ Forex Price Prediction.py
@@ -1,15 +1,14 @@
 import pandas as pd
 import pandas_ta as ta
 
-# Load your data from a CSV file
+
 df = pd.read_csv('GBPUSD.csv')
 
-# Define a function to calculate EMA
+
 def calculate_ema(df, column_name='close', window=12):
     df['EMA'] = df[column_name].ewm(span=window, adjust=False).mean()
     return df
 
-# Define a function to calculate RSI
 def calculate_rsi(df, column_name='close', window=14):
     price_diff = df[column_name].diff(1)
     gain = price_diff.where(price_diff > 0, 0)
@@ -24,28 +23,24 @@ def calculate_rsi(df, column_name='close', window=14):
     df['RSI'] = rsi
     return df
 
-# Calculate EMA and RSI
 df = calculate_ema(df)
 df = calculate_rsi(df)
 
-# Display the DataFrame with added indicators
+
 print(df.head(40))
 
 
-#Splliting Data
 
 from sklearn.model_selection import train_test_split
 
 train_data, test_data = train_test_split(df, test_size=0.1 , random_state = 20)
 
 
-# Fetching X_train and y_train
 X_train = train_data.drop(labels=['close','Date','volume','RSI','EMA'], axis=1)
 
 y_train = train_data['close']
 
 
-# Fetching X_test and y_test
 X_test = test_data.drop(labels=['close', 'Date','volume','RSI','EMA'], axis=1)
 
 
@@ -54,22 +49,18 @@ y_test = test_data['close']
 
 from sklearn.neighbors import KNeighborsRegressor
 
-# Create an instance of the KNeighborsRegressor
-model = KNeighborsRegressor(n_neighbors=5)  # You can adjust the number of neighbors as needed
+model = KNeighborsRegressor(n_neighbors=5)  
 
-# Fit the model to your training data
-model.fit(X_train, y_train)  # X_train is your feature data, y_train is your target data
+model.fit(X_train, y_train)  
 
 
 
-#Predicting Values
 y_test_pred = model.predict(X_test)
 
 
-#Checking accuracy for Training data
 train_accuracy = model.score(X_train, y_train)
 print("Accuracy of train data predictions:", train_accuracy)
 
-#Checking accuracy for Testing data
+
 test_accuracy = model.score(X_test, y_test)
 print("Accuracy of test data predictions:", test_accuracy)
